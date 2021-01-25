156eb936847609a76a6983714d541f0e5425906f
ujos89 <48623147+ujos89@users.noreply.github.com>
2021-01-25 23:35:14 +0900

    Add files via upload

---
+++ leetcode/week4/RemoveElement.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        #traverse
+        before, traverse = None, head
+        while(traverse.val != val):
+            before = traverse
+            traverse = traverse.next
+
+        before.next = traverse.next
+        
+        return head
--- leetcode/week4/ReverseLinkedlist.py
+++ leetcode/week4/ReverseLinkedlist.py
@@ -3,20 +3,15 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def reverseList(self, head):
-        reverse = head
-        head = head.next
-        
-        while(not head):
-            reverse.next = head
+        answer = None
+
+        while(head):
+            tmp = head
             head = head.next
+            tmp.next = answer
+            answer = tmp
+            
+        return answer
         
-        return reverse
-
-
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
