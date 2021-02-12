0cb062905495ff655565637f62b0a5eaee531135
ujos89 <48623147+ujos89@users.noreply.github.com>
2021-02-12 16:04:43 +0900

    Add files via upload

--- baekjoon/algorithm/dynamicprogramming/b1463.py
+++ baekjoon/algorithm/dynamicprogramming/b1463.py
@@ -2,3 +2,28 @@ import sys
 
 n = int(sys.stdin.readline())
 
+# answer = 0
+# while(n!=1):
+#     if n%3 == 0:
+#         n /= 3
+#         answer += 1
+#     elif n%2 == 0:
+#         n /= 2
+#         answer += 1
+#     else:
+#         n -= 1
+#         answer += 1
+
+#     print(answer, n)
+# exception: 10->5->4->2->1, 10->9->3->1
+
+answer = [0, 0]
+for i in range(2, n+1):
+    tmp = [answer[-1]+1]
+    if not i%2:
+        tmp.append(answer[i//2]+1)
+    if not i%3:
+        tmp.append(answer[i//3]+1)
+    answer.append(min(tmp))
+
+print(answer[-1])
